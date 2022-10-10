f2b36c92449686f92bf98ec416492b60a6dbe123
Galitskiy Kirill <9keyoujob9@gmail.com>
2022-10-11 00:49:39 +0300

    Homework done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,28 @@
 ﻿/*
+Проверка на ввод числа.
+"Защита от дурака"
+*/
+
+
+int GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+
+    while(!isCorrect)
+    {
+        isCorrect = int.TryParse(message, out result);
+
+        if(!isCorrect) {
+            Console.WriteLine($"Ввели не число. Введите корректное число\n");
+            message = Console.ReadLine();
+        }
+    }
+
+    return result;
+}
+
+/*
 Задача 1: Напишите программу, 
 которая принимает на вход трёхзначное число и 
 на выходе показывает вторую цифру этого числа.
@@ -52,3 +76,17 @@ ThirdDigit(154865);
 7 -> да
 1 -> нет
 */
+
+void DayOfTheWeek (int day) {
+
+    while (day < 1 || day > 7) {
+        Console.WriteLine($"В неделе семь дней. Введите цифру от 1 до 7\n");
+        day = GetNumber(Console.ReadLine());
+    }
+    if (day > 0 && day < 6)
+        Console.WriteLine($"Сегодня не выходной");
+    else
+        Console.WriteLine($"Сегодня выходной");
+}
+
+DayOfTheWeek(GetNumber(Console.ReadLine()));
